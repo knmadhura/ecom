1de462c2f7c105a16836398372211c896544f0cd
knmadhura <knmadhura2002@gmail.com>
2025-07-17 11:13:58 -0200

    logout.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,18 +1,33 @@
+<?php
+session_start(); // Required to use $_SESSION
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="index.html"><i class="bi bi-bag-check"></i> E-Commerce</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login.html"><i class="bi bi-person-circle"></i>
-                            Login</a></li>
-                </ul>
-            </div>
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="index.php"><i class="bi bi-bag-check"></i> E-Commerce</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link active" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="add-product.php">Add Product</a></li>
+
+                <?php if (empty($_SESSION['id'])): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php"><i class="bi bi-person-circle"></i> Login</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php"><i class="bi bi-box-arrow-right"></i> Logout</a>
+                    </li>
+                <?php endif; ?>
+
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
